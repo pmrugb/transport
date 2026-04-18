01f8c418e98e17b54fb3faf3b53770f4a6e51275
Naseem Shahzad <naseemshahzad@Naseems-MacBook-Pro.local>
2026-04-18 08:57:05 +0500

    updates

--- app/Models/Concerns/PreservesUniqueFieldsOnSoftDelete.php
+++ app/Models/Concerns/PreservesUniqueFieldsOnSoftDelete.php
@@ -2,8 +2,12 @@
 
 namespace App\Models\Concerns;
 
+use Illuminate\Support\Facades\Schema;
+
 trait PreservesUniqueFieldsOnSoftDelete
 {
+    protected static array $softDeleteUniqueFieldLengths = [];
+
     protected static function bootPreservesUniqueFieldsOnSoftDelete(): void
     {
         static::deleted(function ($model): void {
@@ -24,7 +28,7 @@ trait PreservesUniqueFieldsOnSoftDelete
                     continue;
                 }
 
-                $model->setAttribute($field, $value.'__deleted__'.$model->getKey().'_'.time());
+                $model->setAttribute($field, $model->softDeleteUniqueReplacementValue($field, $value));
                 $updated = true;
             }
 
@@ -33,5 +37,46 @@ trait PreservesUniqueFieldsOnSoftDelete
                 $model->saveQuietly();
             }
         });
+    }
+
+    protected function softDeleteUniqueReplacementValue(string $field, string $value): string
+    {
+        $suffix = '__deleted__'.base_convert((string) $this->getKey(), 10, 36).base_convert((string) time(), 10, 36);
+        $maxLength = $this->softDeleteUniqueFieldMaxLength($field);
+
+        if ($maxLength === null) {
+            return $value.$suffix;
+        }
+
+        if ($maxLength <= strlen($suffix)) {
+            return substr(hash('sha256', $this->getTable().'|'.$field.'|'.$value.'|'.$this->getKey().'|'.time()), 0, $maxLength);
+        }
+
+        return substr($value, 0, $maxLength - strlen($suffix)).$suffix;
+    }
+
+    protected function softDeleteUniqueFieldMaxLength(string $field): ?int
+    {
+        $cacheKey = $this->getConnectionName().'|'.$this->getTable().'|'.$field;
+
+        if (array_key_exists($cacheKey, static::$softDeleteUniqueFieldLengths)) {
+            return static::$softDeleteUniqueFieldLengths[$cacheKey];
+        }
+
+        foreach (Schema::connection($this->getConnectionName())->getColumns($this->getTable()) as $column) {
+            if (($column['name'] ?? null) !== $field) {
+                continue;
+            }
+
+            $type = (string) ($column['type'] ?? '');
+
+            if (preg_match('/\((\d+)\)/', $type, $matches) === 1) {
+                return static::$softDeleteUniqueFieldLengths[$cacheKey] = (int) $matches[1];
+            }
+
+            return static::$softDeleteUniqueFieldLengths[$cacheKey] = null;
+        }
+
+        return static::$softDeleteUniqueFieldLengths[$cacheKey] = null;
     }
 }
